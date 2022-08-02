f71cbce01247933f5f95aa04382d194d7f3bfe8e
Vitor Silva <vitor.o.silva95@gmail.com>
2022-08-01 23:59:58 -0300

    style: implements style in stackList component

---
+++ src/components/stackList/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ContainerStacks = styled.div`
+  display:flex;
+  flex-wrap:wrap;
+  align-items:center;
+  justify-content:center;
+  width: 100%;
+`;
