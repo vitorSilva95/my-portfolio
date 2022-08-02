367bd1abb722027230ac381cfead8f53c7af1046
Vitor Silva <vitor.o.silva95@gmail.com>
2022-08-01 23:56:05 -0300

    feat: implements projectList component

---
+++ src/components/projectList/index.jsx
@@ -0,0 +1,34 @@
+import { ContainerCards, Title } from './styled';
+import { useEffect } from 'react';
+import { useScroll } from '../../utils/handleScroll';
+import { dataProjects } from '../../data/data.js';
+import { ProjectCard } from '../projectCard';
+
+export function ProjectCardList() {
+  const [visible, setPixelsValue] = useScroll();
+
+  useEffect(() => {
+    setPixelsValue(500);
+  });
+
+  return (
+    <div>
+      <Title>PROJETOS</Title>
+
+      <ContainerCards visible={visible}>
+        {dataProjects.map(({ id, title, url, about, linkGitHub }) => (
+          <ProjectCard
+            key={id}
+            title={title}
+            url={url}
+            about={about}
+            linkGitHub={linkGitHub}
+          />
+        ))}
+      </ContainerCards>
+      <div id="about" style={{ visibility: 'hidden' }}>
+        about ref
+      </div>
+    </div>
+  );
+}
