86eac573cea7a3f1a8327742ae3a108d2fb0fb0c
Vitor Silva <vitor.o.silva95@gmail.com>
2022-08-01 23:53:55 -0300

    feat: implements footer component

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,46 @@
+import { ContainerFooter, Title, Div, Link, P } from './styled';
+import { dataIcons } from '../../data/data';
+import { HiOutlineLocationMarker } from 'react-icons/hi';
+import { useState } from 'react';
+
+export const Footer = () => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleClipBoard = (url) => {
+    if (url.includes('@gmail.com')) {
+      setIsCopied(true);
+      navigator.clipboard.writeText(url);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 1500);
+    }
+  };
+
+  return (
+    <Div>
+      <Title id="contact">CONTATO</Title>
+      <ContainerFooter>
+        <ol>
+          {dataIcons.map(({ icon, url }, index) => (
+            <li key={index}>
+              <Link
+                href={url.includes('@gmail.com') ? false : url}
+                onClick={() => handleClipBoard(url)}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {icon}
+              </Link>
+            </li>
+          ))}
+        </ol>
+      </ContainerFooter>
+      <P isCopied={isCopied}>E-mail Copiado</P>
+      <h5>
+        <HiOutlineLocationMarker size={40} />
+        Betim , Minas Gerais
+      </h5>
+      <h5>©2021</h5>
+    </Div>
+  );
+};
